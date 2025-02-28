9b2a2af8e230c3ec6663c9d041ef5ed2bdebfafe
mohima-mimino11 <mohimahoque22@gmail.com>
2025-02-28 20:51:36 +0600

    useRef hook concept added

--- src/__form_.js
+++ src/__form_.js
@@ -2,5 +2,5 @@
  * 1. use form tag and onSubmit handler with event (e) from e access e.target.[name of the input field].value 
  * 2. Controlled Element: Use individual field state for each input field
  * 3. Contolled Element: one object in a state holding all the input fields value.
- * 4. Uncontrolled Element
+ * 4. Uncontrolled Element: useRef to create a reference to the element and access value by using like: nameRef.current.value 
  */
