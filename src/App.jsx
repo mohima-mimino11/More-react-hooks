e692f4615649e68c0f97ea864ff84b9adcc61cb5
mohima-mimino11 <mohimahoque22@gmail.com>
2025-03-01 17:54:53 +0600

    reusable components concept added

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,46 @@
 
 import './App.css'
-import HookForm from './components/HookForm/HookForm'
+import ReusableForm from './components/ReusableForm/ReusableForm'
+// import HookForm from './components/HookForm/HookForm'
 // import RefForm from './components/RefForm/RefForm'
 // import StatefulForm from './components/StatefulForm/StatefulForm'
 // import SimpleForm from './components/Forms/SimpleForm'
 
 function App() {
   
+  
+  // handleSignUpSubmit = data =>{
+  //  console.log('sign up', data);
+  // }
 
+  
+  // handleUpdateProfile = data =>{
+  //   // e.preventDefault()
+  //   console.log('update profile', data);
+  // }
   return (
     <>
       
-      <h1>Vite + React</h1>
+      <h1>Form Master</h1>
       {/* <SimpleForm></SimpleForm> */}
       {/* <StatefulForm></StatefulForm> */}
       {/* <RefForm></RefForm> */}
-      <HookForm></HookForm>
+      {/* <HookForm></HookForm> */}
+      {/* <ReusableForm handleSubmit={handleSignUpSubmit}>
+        <div>
+          <h2>Sign Up</h2>
+          <p>Please sign up rn</p>
+        </div>
+      </ReusableForm>
+      <ReusableForm 
+      handleSubmit={handleUpdateProfile}
+      submitBtnText={'Update'}>
+        <div>
+          <h2>Update Profile</h2>
+          <p>Always keep your profile updated</p>
+        </div>
+      </ReusableForm> */}
+
     </>
   )
 }
--- src/__form_.js
+++ src/__form_.js
@@ -3,4 +3,8 @@
  * 2. Controlled Element: Use individual field state for each input field
  * 3. Contolled Element: one object in a state holding all the input fields value.
  * 4. Uncontrolled Element: useRef to create a reference to the element and access value by using like: nameRef.current.value 
+ * 5. use hook to handle states, submit and error. 
+ * example: react hook form 
+ * 
+ * 6. Reusable component
  */
